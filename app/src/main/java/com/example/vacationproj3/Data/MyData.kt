e05356b0f905556f0e67e3e7ae80bf237cd748d8
양현준 <iam@2tle.io>
2021-07-27 21:16:34 +0900

    mydata create

---
+++ app/src/main/java/com/example/vacationproj3/Data/MyData.kt
@@ -0,0 +1,10 @@
+package com.example.vacationproj3.Data
+
+object MyData {
+    var uid : String = ""
+    var displayName : String = ""
+    var photoUrl : String = ""
+    var stressLevel : String = "없음"
+
+
+}
