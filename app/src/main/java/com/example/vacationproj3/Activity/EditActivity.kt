14a753a737bfb5b58118474a2ed4c856edea95b9
JiHyun13 <83816873+JiHyun13@users.noreply.github.com>
2021-08-15 12:45:47 +0900

    오류를 넘기기엔 양심이 찔리지만

--- app/src/main/java/com/example/vacationproj3/Activity/EditActivity.kt
+++ app/src/main/java/com/example/vacationproj3/Activity/EditActivity.kt
@@ -10,7 +10,10 @@ import com.example.vacationproj3.Activity.Community.CommunityActivity
 import com.example.vacationproj3.Function.Firestore
 import com.example.vacationproj3.R
 import com.example.vacationproj3.data.MyData
+import com.example.vacationproj3.data.PostData
 import com.example.vacationproj3.databinding.ActivityEditBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,29 +23,30 @@ class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_edit)
-        val uid = MyData.uid.toString()
+        val text = intent.getStringExtra("text")
+        val postUid = intent.getStringExtra("postUid")
 
-       /* binding.btnEditEd.text =
-        binding.imgPictureEd =
-
-        */
-
+        binding.edWriteEd.setText(text)
         binding.btnEditEd.setOnClickListener{
             CoroutineScope(Dispatchers.Main).launch {
-                val result = Firestore.editPost(uid, binding.edWriteEd.toString())
+                val result = Firestore.editPost(postUid.toString(), binding.edWriteEd.toString())
                 if(result==true){
                     val builder = AlertDialog.Builder(baseContext)
                     builder.setTitle("수정 완료")
                     builder.setPositiveButton("확인") { _: DialogInterface, _:Int ->
                         startActivity(Intent(baseContext, CommunityActivity::class.java))
                     }
-
+                    builder.show()
                 }
                 else{
                     val builder = AlertDialog.Builder(baseContext)
                     builder.setTitle("수정 실패")
+                    builder.setPositiveButton("확인") { _: DialogInterface, _:Int ->
+                    }
+                    builder.show()
                 }
             }
         }
+
     }
 }
--- app/src/main/java/com/example/vacationproj3/Adapter/PostAdapter.kt
+++ app/src/main/java/com/example/vacationproj3/Adapter/PostAdapter.kt
@@ -18,10 +18,12 @@ import com.example.vacationproj3.Activity.EditActivity
 import com.example.vacationproj3.Activity.MainActivity
 import com.example.vacationproj3.Function.Firestore
 import com.example.vacationproj3.data.MyData
+import com.example.vacationproj3.data.MyData.uid
 import com.example.vacationproj3.data.PostData
 import com.example.vacationproj3.databinding.ItemBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.components.ComponentRuntime
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.CoroutineScope
@@ -55,10 +57,12 @@ class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapt
             binding.post = data
             //Glide 통해서 사진 넣어주기
             val storage = Firebase.storage.reference
-            val uid = MyData.uid.toString()
+
+            //val uid = db.collection("post").document(uid).toString()
             storage.child("postImages/"+data.postImageUUID).downloadUrl.addOnSuccessListener {
                 Glide.with(context).load(it).into(binding.img)
             }
+
             //포스트 주인이면 수정삭제 버튼 활성화 아니면 비활성화 if문 분리
             if(MyData.uid.equals(data.writerUid)) {
                 binding.edit.visibility = View.VISIBLE
@@ -68,37 +72,56 @@ class PostAdapter(private val context: Context) : RecyclerView.Adapter<PostAdapt
                 binding.edit.visibility = View.GONE
                 binding.delete.visibility = View.GONE
             }
-            binding.delete.setOnClickListener{
-                CoroutineScope(Dispatchers.Main).launch {
-                    val result = Firestore.delPost(uid)
-                    if (result == true) {
-                        val builder = AlertDialog.Builder(context)
-                        builder.setTitle("삭제 완료")
-                    } else {
-                        val builder = AlertDialog.Builder(context)
-                        builder.setTitle("삭제 오류")
+
+            binding.delete.setOnClickListener{//포스트삭제
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("경고")
+                builder.setMessage("삭제하시겠습니까?")
+                builder.setPositiveButton("확인") {_:DialogInterface, _:Int ->
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val result = Firestore.delPost(data.writerUid)
+                        if (result == true) {
+                            val builder = AlertDialog.Builder(context)
+                            builder.setTitle("삭제 완료")
+                            builder.setMessage("삭제가 성공적으로 완료됐습니다.")
+                            builder.setPositiveButton("확인") {_:DialogInterface, _:Int ->
+                            }
+                            builder.show()
+                        } else {
+                            val builder = AlertDialog.Builder(context)
+                            builder.setTitle("삭제 오류")
+                            builder.setMessage("문제가 생겼습니다. 나중에 다시 시도해주세요.")
+                            builder.setPositiveButton("확인") {_:DialogInterface, _:Int ->
+                            }
+                            builder.show()
+                        }
                     }
+
                 }
-
+                builder.setNegativeButton("취소") { _: DialogInterface, _: Int ->
+                }
+                builder.show()
             }
 
-            binding.edit.setOnClickListener{
-                context.startActivity(Intent(context, EditActivity::class.java))
+            binding.edit.setOnClickListener{//포스트수정
+                val intent = Intent(context, EditActivity::class.java)
+                intent.putExtra("text", data.text)
+                intent.putExtra("postUid", data.postUid)
+                context.startActivity(intent)
             }
 
-            binding.heartBtn.setOnClickListener {
+            //if(data.heart = MyData.uid){//좋아요버튼 초기설정
+                //binding.heartBtn.setBackgroundColor(Color.parseColor("#000000"))
+            //}
+
+            binding.heartBtn.setOnClickListener {//좋아요 클릭시
                 CoroutineScope(Dispatchers.Main).launch {
                     val result : Boolean? = Firestore.heartButton(data.postUid)
                     if (result==true){
                         binding.heartBtn.setBackgroundColor(Color.parseColor("#000000"))
-                        Toast.makeText(context, "추가",Toast.LENGTH_SHORT).show()
                     }
                     else if (result == false){
                         binding.heartBtn.setBackgroundColor(Color.parseColor("#D3D3D3"))
-                        Toast.makeText(context, "삭제",Toast.LENGTH_SHORT).show()
-                    }
-                    else{
-                        Toast.makeText(context, "오류",Toast.LENGTH_SHORT).show()
                     }
 
                 }
